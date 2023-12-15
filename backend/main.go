8801a4dcfe2f68dc2f4ad301b0b89018a5dcbb06
AsanSmailov <144768613+AsanSmailov@users.noreply.github.com>
2023-12-15 13:29:51 +0300

    Update main.go

--- backend/main.go
+++ backend/main.go
@@ -13,9 +13,7 @@ func main() {
 	// Регистрируем маршруты
 	router.HandleFunc("/reg", users.Registration)
 	router.HandleFunc("/auth", users.Authorisation)
-	router.HandleFunc("/add_bank_data", users.Add_bank_data)
 	router.HandleFunc("/add_to_cart", users.Add_to_cart)
-	router.HandleFunc("/check_bank_data", users.Check_bank_data)
 	router.HandleFunc("/sale", users.Sale)
 	router.HandleFunc("/bought", users.Bought)
 	http.ListenAndServe(":8080", router)
